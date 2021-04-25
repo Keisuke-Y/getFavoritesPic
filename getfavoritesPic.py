684470d3c7b8b9c694c1553d59a50ab319924ed7
nesosuke <neso@neso.tech>
2021-04-25 21:18:27 +0900

    複数画像対応，動画対応

--- getfavoritesPic.py
+++ getfavoritesPic.py
@@ -27,8 +27,8 @@ import os
 # API用のキー
 CUSTOMER_KEY = "FfYhUgNHZFvwtdJTcihFTkEJE"
 CUSTOMER_SECRET = "fRFvuSSbY1PjiSnd5stJECSqovtuA6sZ1rMDLLzvcfYIzUBmBa"
-ACCESS_TOKEN = "106067637-GU5BXqQPb1NZn2YrMSmGcriEclW3ANATdMLXFI6y"
-ACCESS_TOKEN_SECRET = "YNcZJDMv7UzOTaWsBrj59HPAdnyBW2wxh1srQrdWw1WAe"
+ACCESS_TOKEN = "106067637-SFNBklOsLgDpLm4X8kkrtQr2Xlm1BhR6BFfEWX8S"
+ACCESS_TOKEN_SECRET = "AHoLBIyV1V0bHwCKvJcp75XreWMppr5HfvVaNTOlnB1FH"
 
 # 走査対象のscreen_nameとツイート数を指定
 target_user = 'nesosuke'
@@ -51,28 +51,66 @@ api = tweepy.API(auth)
 # 1 いいね取得
 res = api.favorites(target_user, count=getcount)
 print(len(res))
+
+# 保存用ディレクトリがなければ作成
+if not os.path.exists(SAVE_DIR):
+    os.makedirs(SAVE_DIR)
+
 # 2 各ツイートごとに画像確認
 for twi in res:
     time.sleep(1)
     # 2-1 画像の有無確認
-    if 'media' in twi._json['entities']:
-        # 2-2 複数画像に対応
-        for picData in twi._json['entities']['media']:
-            # 2-3 画像かどうか確認
-            if picData['type'] == 'photo':
-                # 2-4 ファイル名決定(URLの最後の「/」以降)
-                # 2-4-1 保存用ディレクトリがなければ作成
-                if not os.path.exists(SAVE_DIR):
-                    os.makedirs(SAVE_DIR)
-                fileName = format(
-                    twi.user._json['screen_name']) + '-' + picData['media_url_https'].split("/")[-1]
-                filePath = SAVE_DIR + fileName
-                # 2-5 画像取得Req
-                req = requests.get(picData['media_url_https'])
-                # 2-6 200Rsp　正常時
+
+    if 'extended_entities' in twi._json:
+        for media_data in twi._json['extended_entities']['media']:
+            # 複数画像対応
+            if media_data['type'] == 'photo':
+                filename = format(
+                    twi.user._json['screen_name']) + '-' + media_data['media_url_https'].split("/")[-1]
+                filepath = SAVE_DIR+filename
+                req = requests.get(media_data['media_url_https'])
                 if req.status_code == 200:
-                    # 2-7 ファイル保存
-                    f = open(filePath, 'wb')
+                    f = open(filepath, 'wb')
                     f.write(req.content)
                     f.close()
-                    print(picData['media_url_https'])
+
+            # 動画対応
+            if media_data['type'] == 'video':
+                video_variants = media_data['video_info']['variants']
+                bitrates = []
+                for variants in video_variants:
+                    if 'bitrate' in variants:
+                        bitrates.append(variants['bitrate'])
+
+                video_url = video_variants[bitrates.index(
+                    max(bitrates))]['url']
+                filename = format(
+                    twi.user._json['screen_name']) + '-' + video_url.split("/")[-1].split('?')[-2]
+                filepath = SAVE_DIR+filename
+                req = requests.get(video_url)
+                if req.status_code == 200:
+                    f = open(filepath, 'wb')
+                    f.write(req.content)
+                    f.close()
+
+            print(filename)
+
+            # if 'media' in twi._json['entities']:
+            #     # 2-2 複数画像に対応
+            #     for picData in twi._json['entities']['media']:
+            #         # 2-3 画像かどうか確認
+            #         if picData['type'] == 'photo':
+            #             # 2-4 ファイル名決定(URLの最後の「/」以降)
+
+            #             fileName = format(
+            #                 twi.user._json['screen_name']) + '-' + picData['media_url_https'].split("/")[-1]
+            #             filePath = SAVE_DIR + fileName
+            #             # 2-5 画像取得Req
+            #             req = requests.get(picData['media_url_https'])
+            #             # 2-6 200Rsp　正常時
+            #             if req.status_code == 200:
+            #                 # 2-7 ファイル保存
+            #                 f = open(filePath, 'wb')
+            #                 f.write(req.content)
+            #                 f.close()
+            #                 # print(picData['media_url_https'])
